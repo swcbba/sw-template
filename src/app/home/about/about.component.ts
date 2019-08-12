46385a49afae7dd977577dcd7438f932e2255fd0
Mauricio Arce <mauricio.arce.mat@gmail.com>
2019-08-12 18:08:24 -0400

    Finish initial about us home section design

--- src/app/home/about/about.component.ts
+++ src/app/home/about/about.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SW_NAME } from '../../shared/constants/event-data.constant';
+
 @Component({
   selector: 'sw-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  swName = SW_NAME;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/shared/constants/event-data.constant.ts
+++ src/app/shared/constants/event-data.constant.ts
@@ -1,3 +1,4 @@
+export const SW_NAME = 'Techstars Global Startup Weekend';
 export const EVENT_NAME = 'Startup Weekend Women';
 export const EVENT_DATE = '20-09-2019 17:00';
 export const REGISTRATION_FORM_URL = '';
