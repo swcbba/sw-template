c08809e7565004f512e20f6fc3a991af503f05f5
DentVega <dentvega6@gmail.com>
2019-08-21 10:12:18 -0400

    - corrections

--- src/app/shared/constants/mocks.ts
+++ src/app/shared/constants/mocks.ts
@@ -1,6 +1,6 @@
 import { Event } from '../models/event.model';
 
-export const mockEvents: Event[] =  [
+export const mockEvents: Event[] = [
   {
     id: '2zzgEeRPgWWymAWfjPo3',
     name: 'Workshop No. 1',
@@ -8,14 +8,14 @@ export const mockEvents: Event[] =  [
     place: 'Auditorio Universidad UPAL (Campus Tupac Amaru)',
     date: new Date('2019-01-18')
   },
-   {
+  {
     id: 'dWtfM4NyX4C02KE8QMpr',
     name: 'Workshop No. 2',
     link: 'https://www.facebook.com/events/845727749143044/?ti=ia',
     place: 'Sala COWORK Cafe (Fidel Anze)',
     date: new Date('2019-01-18')
   },
-   {
+  {
     id: 'JeDL2puDlQT5mzBitq90',
     name: 'Workshop No. 3',
     link: 'https://www.facebook.com/events/1345292052301500/?ti=ia',
