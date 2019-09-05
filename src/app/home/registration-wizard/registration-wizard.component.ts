41bb353cd837f6689abcb5f0fe4ae6f89078063c
Griss Garcia <grissgarcia74@gmail.com>
2019-09-05 00:34:41 -0400

    Add form button

--- src/app/home/registration-wizard/registration-wizard.component.ts
+++ src/app/home/registration-wizard/registration-wizard.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { REGISTRATION_FORM_URL } from '../../shared/constants/event-data.constant';
+
 @Component({
   selector: 'sw-registration-wizard',
   templateUrl: './registration-wizard.component.html',
@@ -7,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class RegistrationWizardComponent {
   isLinear = true;
+  registrationFormUrl = REGISTRATION_FORM_URL;
 }
