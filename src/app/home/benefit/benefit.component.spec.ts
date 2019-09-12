5d8d6abc28bd2b87a6d5227ff02335b185e70610
Lizzy Mendivil <lizzy.mendivil@jalasoft.com>
2019-09-12 11:38:47 -0400

    Create new component

---
+++ src/app/home/benefit/benefit.component.spec.ts
@@ -0,0 +1,36 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import {
+  TranslateModule,
+  TranslateLoader,
+  TranslateFakeLoader
+} from '@ngx-translate/core';
+
+import { BenefitComponent } from './benefit.component';
+
+describe('BenefitComponent', () => {
+  let component: BenefitComponent;
+  let fixture: ComponentFixture<BenefitComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BenefitComponent ],
+      imports: [
+        TranslateModule.forRoot({
+          loader: { provide: TranslateLoader, useClass: TranslateFakeLoader }
+        })
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BenefitComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
